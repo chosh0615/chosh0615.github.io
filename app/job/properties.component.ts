b3bfbc97275b3892b441580b0440b65514912490
Sean Cho <ghost897@hanmail.net>
2016-10-06 00:06:12 +0900

    Edited job page frame

--- app/job/properties.component.ts
+++ app/job/properties.component.ts
@@ -10,32 +10,42 @@ import { Property } from        '../model/property';
 @Component({
     selector: 'properties',
     template: `
-        <div style="width:50%; height:50%; background-color: #F7F7F7; float:left; overflow-y:scroll">
-            <h3>Action configuration</h3>
-            <form>
-                <div class="form-group row">
-                    <label for="sourceType" class="col-xs-2 col-form-label">Source type</label>
-                    <div class="col-xs-10">
-                        <select id="sourceType" class="form-control" (change)="onSourceTypeChange($event)">
-                            <option value="">--Select--</option>
-                            <option *ngFor="let construct of sourceConstructs" value="{{ construct.className }}">{{ construct.displayName }}</option>
-                        </select>
-                    </div>
+        <div style="width:50%; height:80%; background-color: #F7F7F7; float:left; display:table">
+            <div style="height:0px; display:table-row;">
+                <h3>Action configuration</h3>
+            </div>
+            <div style="width:100%; display:table-row;">
+                <div style="width:60%; height:100%; overflow-y:scroll; float:left;">
+                    <form>
+                        <div class="form-group row">
+                            <label for="sourceType" class="col-xs-2 col-form-label">Source type</label>
+                            <div class="col-xs-10">
+                                <select id="sourceType" class="form-control" (change)="onSourceTypeChange($event)">
+                                    <option value="">--Select--</option>
+                                    <option *ngFor="let construct of sourceConstructs" value="{{ construct.className }}">{{ construct.displayName }}</option>
+                                </select>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row" *ngFor="let prop of sourceProperties">
+                            <label for="name" class="col-xs-2 col-form-label">{{ prop.name }}</label>
+                            <div class="col-xs-10">
+                                <input type="text" class="form-control" id="name" value="{{ prop.defaultValue }}" placeholder="{{ prop.placeholder }}"/>
+                            </div>
+                        </div>
+
+                        <button type="button" class="btn btn-default" (click)="onSave()">Save</button>
+                    </form>
                 </div>
-
-
-                <div class="form-group row" *ngFor="let prop of sourceProperties">
-                    <label for="name" class="col-xs-2 col-form-label">{{ prop.name }}</label>
-                    <div class="col-xs-10">
-                        <input type="text" class="form-control" id="name" value="{{ prop.defaultValue }}" placeholder="{{ prop.placeholder }}"/>
-                    </div>
+                <div style="width:40%; height:100%; background-color:#888888; overflow-y:scroll; float:left;">
+                    Constructs...
                 </div>
+            </div>
 
-                <button type="button" class="btn btn-default" (click)="onSave()">Save</button>
-            </form>
-        </div>
-        <div style="width:50%;height:30%; background-color:#FFFFEE; float:left">
-            sampling area...
+            <div style="width:100%;height:35%; display:table-row; background-color:#909090;">
+                sampling area...
+            </div>
         </div>
         `
 })
